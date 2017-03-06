2ccb78450f572a1a945567d4c0de7747fdc435d6
Mehdi Druon <mehdidruon@gmail.com>
2017-03-06 12:16:29 +0100

    Modifs footer

--- ThomasCook/src/footer/footer.js
+++ ThomasCook/src/footer/footer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {FooterJS} from './Footer.css';
+import './footer.css';
 
 
 class Footer extends Component {
@@ -26,96 +26,97 @@ class Footer extends Component {
           <div className="catalogues">
             <h4>NOS CATALOGUES</h4>
             <img className="imgCatalogue" src="http://belresources.interactiv-doc.fr//catalogues/thomas-cook-voyages-ete_pdf_440/pages/page1_low.png" alt=""/>
-            <a href="">Toutes nos brochures</a>
+            <a href="" className="linkFooter">Toutes nos brochures</a>
           </div>
           <div className="aPropos">
             <h4>A PROPOS DE THOMAS HOOK</h4>
-            <a href="">Qui sommes-nous ?</a>
-            <a href="">Presse</a>
-            <a href="">Recrutement</a>
-            <a href="">Notre programme d'affiliation</a>
-            <a href="">Compagnies aeriennes</a>
-            <a href="">Voyager en groupe</a>
-            <a href="">Chèques cadeaux</a>
-            <a href="">Chèques vacances ANCV</a>
-            <a href="">Conseils aux voyageurs</a>
+            <a href="" className="linkFooter">Qui sommes-nous ?</a>
+            <a href="" className="linkFooter">Presse</a>
+            <a href="" className="linkFooter">Recrutement</a>
+            <a href="" className="linkFooter">Notre programme d'affiliation</a>
+            <a href="" className="linkFooter">Compagnies aeriennes</a>
+            <a href="" className="linkFooter">Voyager en groupe</a>
+            <a href="" className="linkFooter">Chèques cadeaux</a>
+            <a href="" className="linkFooter">Chèques vacances ANCV</a>
+            <a href="" className="linkFooter">Conseils aux voyageurs</a>
           </div>
           <div className="autreSites">
             <h4>NOS AUTRES SITES</h4>
             <ul>
-              <li><a href="">Jet tours</a></li>
-              <li><a href="">Voyages directs</a></li>
-              <li><a href="">Espace Pro Jet Tours</a></li>
-              <li><a href="">Thomas Cook Marathons</a></li>
-              <li><a href="">Thomas Cook Group</a></li>
-              <li><a href="">Thomas Cook Belgique</a></li>
-              <li><a href="">Thomas Cook Allemagne</a></li>
-              <li><a href="">Thomas Cook Nederland</a></li>
-              <li><a href="">Thomas Cook Pologne</a></li>
-              <li><a href="">Thomas Cook Angleterre</a></li>
+              <li><a href="" className="linkFooter">Jet tours</a></li>
+              <li><a href="" className="linkFooter">Voyages directs</a></li>
+              <li><a href="" className="linkFooter">Espace Pro Jet Tours</a></li>
+              <li><a href="" className="linkFooter">Thomas Cook Marathons</a></li>
+              <li><a href="" className="linkFooter">Thomas Cook Group</a></li>
+              <li><a href="" className="linkFooter">Thomas Cook Belgique</a></li>
+              <li><a href="" className="linkFooter">Thomas Cook Allemagne</a></li>
+              <li><a href="" className="linkFooter">Thomas Cook Nederland</a></li>
+              <li><a href="" className="linkFooter">Thomas Cook Pologne</a></li>
+              <li><a href="" className="linkFooter">Thomas Cook Angleterre</a></li>
             </ul>
           </div>
         </div>
         <div className="footerNav">
           <h4>IDÉES VOYAGES</h4>
-          <i className="fa fa-angle-down" aria-hidden="true"></i>
+          <i className="fa fa-angle-down btnMenu" id="btnMenuDeroulant" aria-hidden="true"></i>
           <h4>OFFRES SPÉCIALES</h4>
-          <i className="fa fa-angle-down" aria-hidden="true"></i>
+          <i className="fa fa-angle-down btnMenu" aria-hidden="true"></i>
           <h4>TOP DESTINATIONS</h4>
-          <i className="fa fa-angle-down" aria-hidden="true"></i>
+          <i className="fa fa-angle-down btnMenu" aria-hidden="true"></i>
           <h4>OÙ ET QUAND PARTIR</h4>
-          <i className="fa fa-angle-down" aria-hidden="true"></i>
+          <i className="fa fa-angle-down btnMenu" aria-hidden="true"></i>
         {/* Créer des liste pour chaque h4 dans une div display none et animationJS quand bouton est cliqué */}
-        </div>
         <div id="menuDeroulant">
           <div className="menuIdeesVoyages">
-            <a href="">Collection été Thomas Cook</a>
-            <a href="">Voyagez avec Jet tours</a>
-            <a href="">Les clubs Jet tour</a>
-            <a href="">Vacances au Maroc</a>
-            <a href="">La Costa Barcelona</a>
-            <a href="">Voyagez en Grèce</a>
-            <a href="">Nos séjours Thalasso</a>
-            <a href="">Nos séjours Golf</a>
-            <a href="">Nos séjours Plongée</a>
-            <a href="">Certificats d'excellence</a>
-            <a href="">Tripadvisor</a>
-            <a href="">Club Jet tours Cruise</a>
+            <a href="" className="linkFooter">Collection été Thomas Cook</a>
+            <a href="" className="linkFooter">Voyagez avec Jet tours</a>
+            <a href="" className="linkFooter">Les clubs Jet tour</a>
+            <a href="" className="linkFooter">Vacances au Maroc</a>
+            <a href="" className="linkFooter">La Costa Barcelona</a>
+            <a href="" className="linkFooter">Voyagez en Grèce</a>
+            <a href="" className="linkFooter">Nos séjours Thalasso</a>
+            <a href="" className="linkFooter">Nos séjours Golf</a>
+            <a href="" className="linkFooter">Nos séjours Plongée</a>
+            <a href="" className="linkFooter">Certificats d'excellence</a>
+            <a href="" className="linkFooter">Tripadvisor</a>
+            <a href="" className="linkFooter">Club Jet tours Cruise</a>
           </div>
           <div className="menuOffresSpeciales">
-            <a href="">Tout à -50%</a>
-            <a href="">Séjours en promo</a>
-            <a href="">Circuits en promo</a>
-            <a href="">Déstockage vols</a>
-            <a href="">Bons plans du moment</a>
-            <a href="">Spécial USA</a>
-            <a href="">Livres photos de vacances</a>
+            <a href="" className="linkFooter">Tout à -50%</a>
+            <a href="" className="linkFooter">Séjours en promo</a>
+            <a href="" className="linkFooter">Circuits en promo</a>
+            <a href="" className="linkFooter">Déstockage vols</a>
+            <a href="" className="linkFooter">Bons plans du moment</a>
+            <a href="" className="linkFooter">Spécial USA</a>
+            <a href="" className="linkFooter">Livres photos de vacances</a>
           </div>
           <div className="menuTopDestinations">
-            <a href="">Séjour Maroc</a>
-            <a href="">Séjour Corse</a>
-            <a href="">Séjour Cuba</a>
-            <a href="">Weekend Amsterdam</a>
-            <a href="">Weekend Barcelone</a>
-            <a href="">Circuit Thailande</a>
-            <a href="">Circuit Vietnam</a>
-            <a href="">Tous nos voyages</a>
+            <a href="" className="linkFooter">Séjour Maroc</a>
+            <a href="" className="linkFooter">Séjour Corse</a>
+            <a href="" className="linkFooter">Séjour Cuba</a>
+            <a href="" className="linkFooter">Weekend Amsterdam</a>
+            <a href="" className="linkFooter">Weekend Barcelone</a>
+            <a href="" className="linkFooter">Circuit Thailande</a>
+            <a href="" className="linkFooter">Circuit Vietnam</a>
+            <a href="" className="linkFooter">Tous nos voyages</a>
           </div>
           <div className="menuOuPartir">
-            <a href="">Vacances d'hiver</a>
-            <a href="">Vacances de Noël</a>
-            <a href="">Vacances du Nouvel An</a>
-            <a href="">Vacances de Février</a>
-            <a href="">Week-end Saint-Valentin</a>
-            <a href="">Vacances de Pâques 2016</a>
-            <a href="">Ponts de mai 2016</a>
-            <a href="">Vacances d'été 2016</a>
-            <a href="">Vacances de Toussaint</a>
-            <a href="">Weekend-end du 11 novembre</a>
-            <a href="">Partir au ski</a>
-            <a href="">Partez de chez vous</a>
+            <a href="" className="linkFooter">Vacances d'hiver</a>
+            <a href="" className="linkFooter">Vacances de Noël</a>
+            <a href="" className="linkFooter">Vacances du Nouvel An</a>
+            <a href="" className="linkFooter">Vacances de Février</a>
+            <a href="" className="linkFooter">Week-end Saint-Valentin</a>
+            <a href="" className="linkFooter">Vacances de Pâques 2016</a>
+            <a href="" className="linkFooter">Ponts de mai 2016</a>
+            <a href="" className="linkFooter">Vacances d'été 2016</a>
+            <a href="" className="linkFooter">Vacances de Toussaint</a>
+            <a href="" className="linkFooter">Weekend-end du 11 novembre</a>
+            <a href="" className="linkFooter">Partir au ski</a>
+            <a href="" className="linkFooter">Partez de chez vous</a>
           </div>
         </div>
+        </div>
+
         <div className="footerPaiement">
           <div className="footerPaiementLeft">
             <p>Paiement 100% sécurisés</p>
@@ -126,7 +127,6 @@ class Footer extends Component {
             <img src="http://www.thomascook.fr/fileadmin/image/paiements.png" alt=""/>
           </div>
         </div>
-        <script type="text/javascript" src={FooterJS}></script>
       </div>
     );
   }
